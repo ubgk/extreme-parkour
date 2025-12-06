48186ed991cf230814caa4af5a17cf79ea0439f0
ubgk <ubgk@users.noreply.github.com>
2025-12-06 02:29:49 +0100

    update camera pos

--- legged_gym/legged_gym/envs/base/legged_robot_config.py
+++ legged_gym/legged_gym/envs/base/legged_robot_config.py
@@ -92,7 +92,8 @@ class LeggedRobotCfg(BaseConfig):
         camera_terrain_num_rows = 10
         camera_terrain_num_cols = 20
 
-        position = [0.27, 0, 0.03]  # front camera
+        # TODO(Bora): update camera parameters
+        position = [0.3, 0, 0.08]  # front camera
         angle = [-5, 5]  # positive pitch down
 
         update_interval = 5  # 5 works without retraining, 8 worse
